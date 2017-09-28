bca0e7f68e643529be9d66419311fca2bffbc9ee
Ahmed Alani <s3leha@gmail.com>
2017-09-28 23:03:29 +0000

    cleaned knex code

--- test_script.js
+++ test_script.js
@@ -5,45 +5,8 @@ const nameTest = process.argv[2];
 
 knex('famous_people')
   .select('*')
-  .where('last_name', 'like', '%' + nameTest + '%').orWhere('first_name', 'like', '%' + nameTest + '%')
+  .where('last_name', nameTest).orWhere('first_name', nameTest)
   .then(console.log);
 
 
 
-
-
-// //return a string query, used place holders to prevent sql injections
-// const myQuery = () => {
-//   return `
-//     SELECT * 
-//     FROM  famous_people
-//     WHERE last_name = $1 
-//     OR    first_name = $1;
-//   `
-// }
-
-
-// const displayResultsFromDb = (err, result) => {
-//   if (err) {
-//     return console.error("error running query", err);
-//   }
-//   console.log(result.rows); //output: 1
-//   client.end();
-// }
-
-
-// const runSql = (q, inputs, cb) => {
-//   return client.query(q, inputs, cb);
-// }
-
-// //connect to pg db
-// client.connect((err) => {
-//   if (err) {
-//     return console.error("Connection Error", err);
-//   }
-//   //take the third item typed into command line
-//   let nameTest = process.argv[2];
-//   //run the query using the input returns the result to the cb
-//   runSql(myQuery(), [nameTest], displayResultsFromDb)
-
-// });
