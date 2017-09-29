4a342aaf5874f0a72a3746f23094c81e87f68604
Ahmed Alani <s3leha@gmail.com>
2017-09-29 01:30:00 +0000

    test1

---
+++ migrations/20170929004346_add_famous_person_id_to_milestones.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+	return Promise.all([
+    knex.schema.alterTable('milestones', function(table) {
+			table.integer('famous_people_id').references('id').inTable('famous_people');
+		})
+	])
+};
+
+exports.down = function(knex, Promise) {
+	return Promise.all([
+		knex.schema.table('milestones',function(table){
+			table.dropColumn('famous_people_id')
+		})
+  ]);
+};
